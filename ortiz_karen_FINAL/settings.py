0cfdf4dd45dc2433c4ed463917811fab52342875
Karen Sofia Ortiz Silva <134814147+karen-sofia@users.noreply.github.com>
2023-12-22 23:16:28 -0300

    prueba final

--- ortiz_karen_FINAL/settings.py
+++ ortiz_karen_FINAL/settings.py
@@ -74,15 +74,16 @@ WSGI_APPLICATION = 'ortiz_karen_FINAL.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 import pymysql
-pymysql.install_as_MySQLdb
+pymysql.install_as_MySQLdb()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'DJANGO_SEMINARIO',
-        'USER' : 'root',
-        'PASSWORD' : ''
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
--- seminario_app/forms.py
+++ seminario_app/forms.py
@@ -5,19 +5,9 @@ class FormInstituciones(forms.ModelForm):
     class Meta:
         model = Instituciones
         fields = '__all__'
-        widgets = {
-            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
-        }
-
+        
 class FormInscritos(forms.ModelForm):
     class Meta:
         model = Inscritos
         fields = ['id', 'nombre', 'telefono', 'institucion', 'estado', 'observacion']
-        widgets = {
-            'id': forms.NumberInput(attrs={'class': 'form-control'}),
-            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
-            'telefono': forms.TextInput(attrs={'class': 'form-control'}),
-            'institucion': forms.Select(attrs={'class': 'form-select'}),
-            'estado': forms.Select(attrs={'class': 'form-select'}),
-            'observacion': forms.TextInput(attrs={'class': 'form-control'}),
-        }
+        
